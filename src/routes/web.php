bf21c4623c9bfe1cdabc2cc354a655a5cc254bd2
Francesco Passanante <francescopassanante@gmail.com>
2023-08-31 14:56:32 +0200

    fix routing

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,26 +2,28 @@
 use Illuminate\Support\Facades\Route;
 use Fieroo\Exhibitors\Controllers\ExhibitorController;
 
-Route::get('/compile-data', [ExhibitorController::class, 'compileData'])->name('compile-data-after-login');
-Route::get('/pending-admission', [ExhibitorController::class, 'pendingAdmission'])->name('pending-admission');
+Route::group(['middleware' => ['web','auth']], function() {
+    Route::get('/compile-data', [ExhibitorController::class, 'compileData'])->name('compile-data-after-login');
+    Route::get('/pending-admission', [ExhibitorController::class, 'pendingAdmission'])->name('pending-admission');
 
-Route::group(['prefix' => 'admin', 'middleware' => ['web','auth']], function() {
-    Route::resource('/exhibitors', ExhibitorController::class);
-    Route::get('/exhibitors-incomplete', [ExhibitorController::class, 'incompleteData']);
-    Route::delete('/exhibitors-incomplete/destroy/{id}', [ExhibitorController::class, 'destroyIncomplete'])->name('exhibitors-incomplete.destroy');
-    Route::post('/exhibitors/getAjaxListIncompleted', [ExhibitorController::class, 'getAjaxListIncompleted']);
-    Route::post('/exhibitors/getSelectList', [ExhibitorController::class, 'getSelectList']);
-    Route::post('/exhibitors/getAjaxList', [ExhibitorController::class, 'getAjaxList']);
-    Route::post('/exhibitors/compile-data', [ExhibitorController::class, 'sendFormCompileData'])->name('compile-data');
-    Route::group(['prefix' => 'exhibitor'], function() {
-        Route::get('/{id}/events', [ExhibitorController::class, 'indexEvents']);
-        Route::get('/{id}/event/{event_id}/recap', [ExhibitorController::class, 'recapEvent']);
-        Route::get('/{id}/admit', [ExhibitorController::class, 'admit']);
-    });
+    Route::group(['prefix' => 'admin'], function() {
+        Route::resource('/exhibitors', ExhibitorController::class);
+        Route::get('/exhibitors-incomplete', [ExhibitorController::class, 'incompleteData']);
+        Route::delete('/exhibitors-incomplete/destroy/{id}', [ExhibitorController::class, 'destroyIncomplete'])->name('exhibitors-incomplete.destroy');
+        Route::post('/exhibitors/getAjaxListIncompleted', [ExhibitorController::class, 'getAjaxListIncompleted']);
+        Route::post('/exhibitors/getSelectList', [ExhibitorController::class, 'getSelectList']);
+        Route::post('/exhibitors/getAjaxList', [ExhibitorController::class, 'getAjaxList']);
+        Route::post('/exhibitors/compile-data', [ExhibitorController::class, 'sendFormCompileData'])->name('compile-data');
+        Route::group(['prefix' => 'exhibitor'], function() {
+            Route::get('/{id}/events', [ExhibitorController::class, 'indexEvents']);
+            Route::get('/{id}/event/{event_id}/recap', [ExhibitorController::class, 'recapEvent']);
+            Route::get('/{id}/admit', [ExhibitorController::class, 'admit']);
+        });
 
-    Route::group(['prefix' => 'export'], function() {
-        Route::get('/exhibitors', [ExhibitorController::class, 'exportAll']);
-        Route::get('/exhibitors-incomplete', [ExhibitorController::class, 'exportIncompleted']);
-        Route::get('/exhibitor/{id}/orders', [ExhibitorController::class, 'exportOrders']);
+        Route::group(['prefix' => 'export'], function() {
+            Route::get('/exhibitors', [ExhibitorController::class, 'exportAll']);
+            Route::get('/exhibitors-incomplete', [ExhibitorController::class, 'exportIncompleted']);
+            Route::get('/exhibitor/{id}/orders', [ExhibitorController::class, 'exportOrders']);
+        });
     });
 });
