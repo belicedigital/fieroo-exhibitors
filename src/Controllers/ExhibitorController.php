8dc7ac491692eca519978b89ef2d7d9b973bb7f0
Francesco Passanante <francescopassanante@gmail.com>
2023-08-31 14:49:49 +0200

    remove dump

--- src/Controllers/ExhibitorController.php
+++ src/Controllers/ExhibitorController.php
@@ -30,7 +30,6 @@ class ExhibitorController extends Controller
     public function compileData()
     {
         $auth = Auth::user();
-        dd($auth);
         $user = User::findOrFail($auth->id);
         App::setLocale($user->exhibitor->locale);
         return view('exhibitors::compile-data');
