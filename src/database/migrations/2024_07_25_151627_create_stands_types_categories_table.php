01f80f36cc1dd69dfb0b017eba8bb53c114a149a
Francesco Passanante <francescopassanante@gmail.com>
2024-07-25 17:21:17 +0200

    feat: add migration

---
+++ src/database/migrations/2024_07_25_151627_create_stands_types_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stands_types_categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->unsignedBigInteger('stand_type_id');
+            $table->foreign('stand_type_id')->references('id')->on('stands_types')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stands_types_categories');
+    }
+};
